11e0b070716357c6f8f6c76e610f75176f7322a1
Alberto Fabro <26629510+Eiliv17@users.noreply.github.com>
2022-12-19 09:31:44 +0100

    feat(posts): add post creation endpoint

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Eiliv17/GinJWTAuthAPI/initializers"
 	"github.com/Eiliv17/GinJWTAuthAPI/models"
@@ -69,12 +70,69 @@ func GetPost(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, post)
 }
 
-/* func CreatePost(c *gin.Context) {
+func CreatePost(c *gin.Context) {
 	// database setup
 	dbname := os.Getenv("DB_NAME")
 	coll := initializers.DB.Database(dbname).Collection("posts")
 
-} */
+	// get user info from context
+	rawvalue, exist := c.Get("user")
+	if !exist {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{
+			"error": "not authorized",
+		})
+		return
+	}
+
+	user := rawvalue.(models.User)
+
+	// body data struct
+	body := struct {
+		PostTitle        string   `json:"postTitle" binding:"required"`
+		Tags             []string `json:"tags" binding:"required"`
+		ShortDescription string   `json:"shortDescription" binding:"required"`
+		PostData         string   `json:"postData" binding:"required"`
+	}{}
+
+	// gets the body data
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": "invalid json",
+			"postDocument": gin.H{
+				"postTitle":        "string",
+				"tags":             "[]string",
+				"shortDescription": "string",
+				"postData":         "string",
+			},
+		})
+		return
+	}
+
+	// create the post object
+	timeNow := time.Now()
+	post := models.Post{
+		PostID:           primitive.NewObjectIDFromTimestamp(timeNow),
+		PostTitle:        body.PostTitle,
+		Date:             timeNow,
+		AuthorID:         user.UserID,
+		Tags:             body.Tags,
+		ShortDescription: body.ShortDescription,
+		PostData:         body.PostData,
+	}
+
+	// store the post into the database
+	_, err = coll.InsertOne(context.TODO(), post)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{
+			"error": "could not store into the database",
+		})
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"result": "post created",
+	})
+}
 
 /* func DeletePost(c *gin.Context) {
 	// database setup
--- models/postModel.go
+++ models/postModel.go
@@ -1,10 +1,15 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Post struct {
 	PostID           primitive.ObjectID `bson:"_id" json:"postID"`
 	PostTitle        string             `bson:"postTitle" json:"postTitle"`
+	Date             time.Time          `bson:"date" json:"date"`
 	AuthorID         primitive.ObjectID `bson:"authorID" json:"authorID"`
 	Tags             []string           `bson:"tags" json:"tags"`
 	ShortDescription string             `bson:"shortDescription" json:"shortDescription"`
